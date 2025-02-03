7c6cbd75fe3266c07199b8f643ca8ab06a6e1475
Javier Murgoitio Esandi <murgoiti@usc.edu>
2025-02-03 15:31:42 -0800

    Modifying the launching script

--- launch_main.sh
+++ launch_main.sh
@@ -1,19 +1,3 @@
-#!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=220GB
-#SBATCH --time=48:00:00
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:a100:1
-#SBATCH --account=aoberai_286
-
-module purge
-module load gcc/13.3.0
-module load cuda/12.6.3
-eval "$(conda shell.bash hook)"
-conda activate pytorch_env
-
 tensorboard --logdir=/project/aoberai_286/RCD/FoundationModel_LiquidBiopsy/tensorboard_logs --host=0.0.0.0 --port=6006 &
 
 python3 main.py
